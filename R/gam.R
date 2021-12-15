2522067b6efe179575165cb6df74a3d872db8992
adithirgis <adithi@ilklabs.com>
2021-12-15 13:11:34 +0530

    commit

--- R/gam.R
+++ R/gam.R
@@ -1,7 +1,48 @@
+library(xgboost)
+library(readxl)
+library(tidyverse)
+library(tidymodels)
+library(h2o)
+library(here)
+library(data.table)
+library(mgcv)
+library(caret)
 
-gam_model <- gam(BAM ~ s(PA_CF_1) + s(PA_RH) + s(PA_Temp) + s(hour), data = file_shared)
+set.seed(108)
 
-file_shared$Predicted_Corrected_PM25_PA_both <- predict(gam_model, newdata = file_shared)
+file_shared <- read_csv(here("data", "PurpleAir_BAM_Data.csv")) %>%
+  select("date" = `Date and time (Before time stamp)`,
+         "BAM" = `BAM-PM2.5`, "PA_CF_1" = `PA-PM2.5 (CF=1)`, "PA_CF_ATM" = `PA-PM2.5 (CF=ATM)`,
+         "PA_RH" = `PA-RH`, "PA_Temp" = `PA-Temperature`, BC) %>%
+  mutate(date = as.POSIXct(date, format = "%d-%m-%Y %H:%M", tz = "Asia/Kolkata"),
+         hour = format(date, "%H")) %>%
+  mutate_if(is.character, as.numeric) %>%
+  na.omit() %>%
+  select(BAM, everything(), -date, -BC, -PA_CF_1)
 
-write.csv(file_shared, "gam.csv")
+
+
+# file_shared <- read_csv(here("data", "PurpleAir_BAM_Data.csv")) %>%
+#   select("date" = `Date and time (Before time stamp)`,
+#          "BAM" = `BAM-PM2.5`, "PA_CF_1" = `PA-PM2.5 (CF=1)`, "PA_CF_ATM" = `PA-PM2.5 (CF=ATM)`,
+#          "PA_RH" = `PA-RH`, "PA_Temp" = `PA-Temperature`, BC) %>%
+#   mutate(date = as.POSIXct(date, format = "%d-%m-%Y %H:%M", tz = "Asia/Kolkata"),
+#          hour = format(date, "%H")) %>%
+#   mutate_if(is.character, as.numeric) %>%
+#   na.omit() %>%
+#   select(BAM, everything(), -date, -BC, -PA_CF_ATM)
+
+
+# file_shared$hour <- as.factor(file_shared$hour)
+gam_model <- train(BAM ~ PA_CF_ATM + PA_RH + PA_Temp, 
+           data = file_shared,
+           method = "gam",
+           trControl = trainControl(method = "cv", number = 10, 
+                                    savePredictions = TRUE)
+)
+
+file_shared$Predicted_Corrected_ATM_pred <- predict(gam_model, newdata = file_shared)
+write.csv(file_shared, "gam_CF_ATM.csv")
+
+write.csv(as.data.frame(gam_model$pred), "gam_CF_ATM_CV.csv")
 
