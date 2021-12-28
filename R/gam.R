6632ee91316b0a492dc42d91e05b72065f596267
adithirgis <adithi@ilklabs.com>
2021-12-28 16:34:01 +0530

    commit

--- R/gam.R
+++ R/gam.R
@@ -17,49 +17,37 @@ file_shared <- read_csv(here("data", "PurpleAir_BAM_Data.csv")) %>%
   mutate(date = as.POSIXct(date, format = "%d-%m-%Y %H:%M", tz = "Asia/Kolkata"),
          hour = format(date, "%H")) %>%
   mutate_if(is.character, as.numeric) %>%
-  na.omit() %>%
-  select(BAM, everything(), -date, -BC, -PA_CF_1)
+  select(BAM, everything(), -date, -BC) %>% 
+  na.omit() 
 
 
+file_shared$hour <- as.factor(file_shared$hour)
+gam_model <- train(BAM ~ PA_CF_1 + PA_RH + PA_Temp,
+           data = file_shared,
+           method = "gam",
+           trControl = trainControl(method = "cv", number = 10,
+                                    savePredictions = TRUE)
+)
 
-# file_shared <- read_csv(here("data", "PurpleAir_BAM_Data.csv")) %>%
-#   select("date" = `Date and time (Before time stamp)`,
-#          "BAM" = `BAM-PM2.5`, "PA_CF_1" = `PA-PM2.5 (CF=1)`, "PA_CF_ATM" = `PA-PM2.5 (CF=ATM)`,
-#          "PA_RH" = `PA-RH`, "PA_Temp" = `PA-Temperature`, BC) %>%
-#   mutate(date = as.POSIXct(date, format = "%d-%m-%Y %H:%M", tz = "Asia/Kolkata"),
-#          hour = format(date, "%H")) %>%
-#   mutate_if(is.character, as.numeric) %>%
-#   na.omit() %>%
-#   select(BAM, everything(), -date, -BC, -PA_CF_ATM)
+file_shared$Predicted_Corrected_1_pred <- predict(gam_model, newdata = file_shared)
+write.csv(file_shared, "gam_CF_1.csv")
+gam_cv <- as.data.frame(gam_model$pred)
+gam_cv <- gam_cv %>%
+  subset(select == "FALSE")
+write.csv(gam_cv, "gam_CF_1_CV.csv")
 
 
-# file_shared$hour <- as.factor(file_shared$hour)
-# gam_model <- train(BAM ~ PA_CF_1 + PA_RH + PA_Temp, 
-#            data = file_shared,
-#            method = "gam",
-#            trControl = trainControl(method = "cv", number = 10, 
-#                                     savePredictions = TRUE)
+# gam_model <- train(BAM ~ PA_CF_ATM + PA_RH + PA_Temp, 
+#                    data = file_shared,
+#                    method = "gam",
+#                    trControl = trainControl(method = "cv", number = 10, 
+#                                             savePredictions = TRUE)
 # )
 # 
-# file_shared$Predicted_Corrected_1_pred <- predict(gam_model, newdata = file_shared)
-# write.csv(file_shared, "gam_CF_1.csv")
+# file_shared$Predicted_Corrected_ATM_pred <- predict(gam_model, newdata = file_shared)
+# write.csv(file_shared, "gam_CF_ATM.csv")
 # gam_cv <- as.data.frame(gam_model$pred)
 # gam_cv <- gam_cv %>% 
 #   subset(select == "FALSE")
-# write.csv(gam_cv, "gam_CF_1_CV.csv")
-
-
-gam_model <- train(BAM ~ PA_CF_ATM + PA_RH + PA_Temp, 
-                   data = file_shared,
-                   method = "gam",
-                   trControl = trainControl(method = "cv", number = 10, 
-                                            savePredictions = TRUE)
-)
-
-file_shared$Predicted_Corrected_ATM_pred <- predict(gam_model, newdata = file_shared)
-write.csv(file_shared, "gam_CF_ATM.csv")
-gam_cv <- as.data.frame(gam_model$pred)
-gam_cv <- gam_cv %>% 
-  subset(select == "FALSE")
-write.csv(gam_cv, "gam_CF_ATM_CV.csv")
+# write.csv(gam_cv, "gam_CF_ATM_CV.csv")
 
